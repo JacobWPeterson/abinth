37d44d77b9335723734e09078484f4589320b5ec
Jacob Peterson <petersonjacobw@gmail.com>
2021-03-31 13:18:58 -0700

    Tweet Body Component now filters out URLs from body
    –Asynchronous behavior needs to be modified to re-insert links to non-twitter content

--- client/src/components/TweetBody.jsx
+++ client/src/components/TweetBody.jsx
@@ -13,6 +13,7 @@ const URL = styled.a`
     cursor: pointer;
   };
 `;
+
 const urlPattern = /^(https?|ftp|torrent|image|irc):\/\/(-\.)?([^\s\/?\.#]+\.?)+(\/[^\s]*)?$/i;
 
 // function linkify(text) {
@@ -20,26 +21,123 @@ const urlPattern = /^(https?|ftp|torrent|image|irc):\/\/(-\.)?([^\s\/?\.#]+\.?)+
 //   return text.replace(urlRegex, (url) => <URL href={url} target="_blank">{`${url}`}</URL>);
 // }
 
+function renderText(text) {
+  const parts = text.split(urlPattern);
+  for (let i = 1; i < parts.length; i += 2) {
+    parts[i] = <a key={`link${i}`} href={parts[i]}>{parts[i]}</a>;
+  }
+  return parts;
+}
+
+const findURL = (url) => {
+  const urlStartIndex = url.indexOf('https://');
+  let foundUrl = url.slice(urlStartIndex);
+  const foundUrlEndIndex = foundUrl.indexOf('"');
+  foundUrl = foundUrl.slice(0, foundUrlEndIndex);
+  return foundUrl;
+};
+
+const isTwitterLink = (link) => (link.includes('https://twitter.com/'));
+
+class TweetBody extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      textToRender: null,
+    };
+    this.dissectTweet = this.dissectTweet.bind(this);
+  }
+
+  componentDidMount() {
+    this.dissectTweet(this.props.text, (words) => this.setState({
+      textToRender: words,
+    }));
+  }
+
+  componentDidUpdate(prevProps) {
+    if (prevProps !== this.props) {
+      this.dissectTweet(this.props.text, (words) => this.setState({
+        textToRender: words,
+      }));
+    }
+  }
+
+  async dissectTweet(text, callback) {
+    let words = text.split(' ');
+    words = words.filter((word) => {
+      if (word.match(urlPattern)) {
+        resolveURI(word).then((x) => {
+          // console.log(x.data);
+          const foundUrl = findURL(x.data);
+          if (!isTwitterLink(foundUrl)) {
+            console.log('OutsideURL: ', word);
+            return word;
+          }
+        });
+      } else {
+        return word;
+      }
+    });
+    // console.log(words.join(' '));
+    callback(words.join(' '));
+    console.log(this.state.textToRender);
+  }
+
+  render() {
+    if (this.state.textToRender) {
+      return <div className="some_text_class">{this.state.textToRender}</div>;
+    }
+    return <div />;
+  }
+}
+
+// const urlPattern = /^(https?|ftp|torrent|image|irc):\/\/(-\.)?([^\s\/?\.#]+\.?)+(\/[^\s]*)?$/i;
+
+// // function linkify(text) {
+// eslint-disable-next-line max-len
+// //   const urlRegex = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
+// //   return text.replace(urlRegex, (url) => <URL href={url} target="_blank">{`${url}`}</URL>);
+// // }
+
 // function renderText(text) {
-//   const parts = text.split(urlRegex);
+//   const parts = text.split(urlPattern);
 //   for (let i = 1; i < parts.length; i += 2) {
 //     parts[i] = <a key={`link${i}`} href={parts[i]}>{parts[i]}</a>;
 //   }
 //   return parts;
 // }
 
-const TweetBody = ({ text }) => {
-  const words = text.split(' ');
-  words.forEach((word) => {
-    if (word.match(urlPattern)) {
-      resolveURI(word).then((x) => x.data).then(console.log);
-      // resolveURI(word, (x) => { console.log(x); });
-    }
-  });
-  // const editedText = renderText(text);
-  return (
-    <div className="some_text_class">{text}</div>
-  );
-};
+// const findURL = (url) => {
+//   const urlStartIndex = url.indexOf('https://');
+//   let foundUrl = url.slice(urlStartIndex);
+//   const foundUrlEndIndex = foundUrl.indexOf('"');
+//   foundUrl = foundUrl.slice(0, foundUrlEndIndex);
+//   return foundUrl;
+// };
+
+// const isTwitterLink = (link) => (link.includes('https://twitter.com/'));
+
+// const TweetBody = ({ text }) => {
+//   let words = text.split(' ');
+//   words = words.filter((word) => {
+//     if (word.match(urlPattern)) {
+//       resolveURI(word).then((x) => {
+//         // console.log(x.data);
+//         const foundUrl = findURL(x.data);
+//         if (!isTwitterLink(foundUrl)) {
+//           console.log('OutsideURL: ', word);
+//           return word;
+//         }
+//       });
+//     } else {
+//       return word;
+//     }
+//   });
+//   console.log('words: ', words);
+//   // const editedText = renderText(filteredTextBody.join(' '));
+//   return (
+//     <div className="some_text_class">{text}</div>
+//   );
+// };
 
 export default TweetBody;
