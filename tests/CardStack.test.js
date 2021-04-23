6b803afc90ba92793c767fcc641256c55ab63dfc
Jacob Peterson <petersonjacobw@gmail.com>
2021-04-22 23:46:36 -0700

    Added tests for checking window click navigation
    –changeIndex needs to be mocked still

--- tests/CardStack.test.js
+++ tests/CardStack.test.js
@@ -2,11 +2,21 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import CardStack from '../client/src/components/CardStack.jsx';
+import TweetCard from '../client/src/components/TweetCard.jsx';
 
 describe('CardStack', () => {
+  const component = shallow(<CardStack debug />);
   it('should render correctly in "debug" mode', () => {
-    const component = shallow(<CardStack debug />);
-
     expect(component).toMatchSnapshot();
+  });
+  const clickArea = component.find('div');
+  const tweet = component.find(TweetCard);
+  it('increases index when non-Tweet area is clicked', () => {
+    clickArea.simulate('click');
+    expect(changeIndex.mock.calls.length).toBe(1);
+  });
+  it('index does not increase when Tweet area is clicked', () => {
+    tweet.simulate('click');
+    expect(changeIndex.mock.calls.length).toBe(0);
   });
 });
