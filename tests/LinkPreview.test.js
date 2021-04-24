4a04accb5bbb101bbc1342b45c1eacb1c0e7b862
Jacob Peterson <petersonjacobw@gmail.com>
2021-04-23 22:38:44 -0700

    Added tests to LinkPreview
    –100% coverage for component
    –App Total 45%

--- tests/LinkPreview.test.js
+++ tests/LinkPreview.test.js
@@ -1,16 +1,33 @@
 /* eslint-disable no-undef */
 import React from 'react';
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
 import LinkPreview from '../client/src/components/LinkPreview.jsx';
 
-const props = {
+const props1 = {
   imageUrl: 'www.yahoo.com/12313123123', title: 'Hello World', description: 'This is a news story', link: 'www.yahoo.com/12313123123',
 };
 
-describe('LinkPreview', (props) => {
-  it('should render correctly in "debug" mode', () => {
-    const component = shallow(<LinkPreview debug />);
+const props2 = {
+  imageUrl: 'www.yahoo.com/12313123123', title: 'Hello World', description: 'This is a news story', link: 'www.yahoo.nz/12313123123',
+};
 
+describe('LinkPreview', () => {
+  const component = mount(<LinkPreview {...props1} debug />);
+  it('should render correctly in "debug" mode', () => {
     expect(component).toMatchSnapshot();
+  });
+
+  const linkArea = component.find('div').at(1).children().at(2).text();
+
+  it('should render link correctly when link includes .com', () => {
+    expect(linkArea).toEqual('🔗www.yahoo.com');
+  });
+});
+
+describe('LinkPreview', () => {
+  const component = mount(<LinkPreview {...props2} debug />);
+  const linkArea = component.find('div').at(1).children().at(2).text();
+  it('should render link correctly when link includes .com', () => {
+    expect(linkArea).toEqual(`🔗${props2.link}`);
   });
 });
